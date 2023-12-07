859d975c492a22342653ba591b294575b8ff15d6
benhurst1 <ben-hurst@outlook.com>
2023-12-07 19:26:51 +0000

    testing headless

--- indeed.py
+++ indeed.py
@@ -1,8 +1,22 @@
 from bs4 import BeautifulSoup
 import time, random
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome()
+# driver = webdriver.Chrome()
+
+options = Options()
+user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+options.add_argument(f'user-agent={user_agent}')
+options.add_argument('--no-sandbox')
+options.add_argument('--window-size=1920,1080')
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+options.add_argument('--allow-running-insecure-content')
+options.add_argument("--headless")
+driver = webdriver.Chrome(options=options)
+
+
 
 # job details
 job = 'data'
